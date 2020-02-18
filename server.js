6b9613c49762f120277b4d8fd0335c7be6f72707
sam11385 <samluedke@gmail.com>
2020-02-18 12:41:41 -0700

    Committing mongo app

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect('link-to-mongodb', (err, database) => {
+  // ... start the server
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
